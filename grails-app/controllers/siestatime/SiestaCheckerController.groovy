2d66e4a4543d89d4aa17b47929c6eec0c508835e
Tan Doan <td@tandoan.com>
2014-02-28 05:35:35 -0800

    making call to server to check if it is seista time

--- grails-app/controllers/siestatime/SiestaCheckerController.groovy
+++ grails-app/controllers/siestatime/SiestaCheckerController.groovy
@@ -7,8 +7,26 @@ class SiestaCheckerController {
     }
 
     def check() { 
+    	//Set the timezone to Madrid
+    	TimeZone tz = TimeZone.getTimeZone("GMT+01:00");
+		Calendar c = Calendar.getInstance(tz);
+		Number current_hour = c.get(Calendar.HOUR_OF_DAY);
+
+		Boolean isSiesta = false;
+		//If it's after 2pm and before 5pm, it is definitely siesta time
+		if(current_hour > 13 && current_hour < 17){
+			isSiesta = true;
+		}
+
+		String siestaText;
+		if(isSiesta){
+			siestaText = "Yes"
+		} else {
+			siestaText = "No"
+		}
+
 		render(contentType: "application/json") {
-			is_siesta = true
+			is_siesta = siestaText
 		}
     }
 }
